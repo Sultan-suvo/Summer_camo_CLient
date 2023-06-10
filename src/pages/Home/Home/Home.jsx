fc04ea18509a307dfdd051d55a4c41612a754688
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-06-10 16:04:04 +0600

    toggle theme added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,16 +1,32 @@
-import { Helmet } from "react-helmet-async";
-import Banner from "../Banner/Banner";
-import PopularClasses from "../PopularClasses/PopularClasses";
-import PopularInstructor from "../PopularInstructor/PopularInstructor";
+import { useState } from 'react';
+import { Helmet } from 'react-helmet-async';
+import Banner from '../Banner/Banner';
+import PopularClasses from '../PopularClasses/PopularClasses';
+import PopularInstructor from '../PopularInstructor/PopularInstructor';
+
 const Home = () => {
+    const [isDarkTheme, setIsDarkTheme] = useState(true);
+
+    const toggleTheme = () => {
+        setIsDarkTheme(prevTheme => !prevTheme);
+    };
+
+    const themeColor = isDarkTheme ? 'bg-gray-900 text-white' : 'bg-white text-gray-900';
+
     return (
-        <div>
+        <div className={`home ${themeColor}`}>
             <Helmet>
                 <title>Song Book | Home</title>
             </Helmet>
-           <Banner></Banner>
-           <PopularClasses></PopularClasses>
-           <PopularInstructor></PopularInstructor>
+            <button
+                className="bg-blue-500 fixed z-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={toggleTheme}
+            >
+                Toggle Theme
+            </button>
+            <Banner />
+            <PopularClasses />
+            <PopularInstructor />
         </div>
     );
 };
