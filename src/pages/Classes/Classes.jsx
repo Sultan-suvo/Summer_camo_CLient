d3f96c0236554576a7b001f205f2f858f6e13b5c
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-06-12 23:14:34 +0600

    update classes

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -4,11 +4,15 @@ import { Helmet } from "react-helmet-async";
 import useAuth from "../../hooks/useAuth";
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useAdmin from "../../hooks/useAdmin";
+import useInstructor from "../../hooks/useInstructor";
 
 const Classes = () => {
   const [classes, setClasses] = useState([]);
-  const {user}  =useAuth();
-  const [axiosSecure] = useAxiosSecure()
+  const { user } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
+  const [isAdmin] = useAdmin();
+  const [isInstructor] = useInstructor();
 
   useEffect(() => {
     fetchClasses();
@@ -53,37 +57,37 @@ const Classes = () => {
   };
 
   return (
-   <>
-       <Helmet>
+    <>
+      <Helmet>
         <title>Song Book | Classes</title>
       </Helmet>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-12">
-      {classes.map((classItem) => (
-        <div
-          key={classItem._id}
-          className={`rounded-lg p-4 shadow ${
-            classItem.availableSeats <= 0 ? "bg-red-300" : "bg-white"
-          }`}
-        >
-          <img src={classItem.classImage} alt="Class Image" className="mb-4 h-64 w-full rounded-xl" />
-          <h2 className="text-xl font-bold mb-2">{classItem.className}</h2>
-          <p className="mb-2">Instructor: {classItem.instructorName}</p>
-          <p className="mb-2">Available Seats: {classItem.availableSeats}</p>
-          <p className="mb-4">Price: {classItem.price}</p>
+        {classes.map((classItem) => (
+          <div
+            key={classItem._id}
+            className={`rounded-lg p-4 shadow ${
+              classItem.availableSeats <= 0 ? "bg-red-300" : "bg-white"
+            }`}
+          >
+            <img src={classItem.classImage} alt="Class Image" className="mb-4 h-64 w-full rounded-xl" />
+            <h2 className="text-xl font-bold mb-2">{classItem.className}</h2>
+            <p className="mb-2">Instructor: {classItem.instructorName}</p>
+            <p className="mb-2">Available Seats: {classItem.availableSeats}</p>
+            <p className="mb-4">Price: {classItem.price}</p>
 
-          {classItem.availableSeats > 0 ? (
-            <button onClick={() => handleSelectClass(classItem)} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">
-              Select
-            </button>
-          ) : (
-            <button className="bg-gray-500 cursor-not-allowed text-white px-4 py-2 rounded-lg" disabled>
-              Not Available
-            </button>
-          )}
-        </div>
-      ))}
-    </div>
-   </>
+            {classItem.availableSeats > 0 && !isAdmin && !isInstructor && user?.email ? (
+              <button onClick={() => handleSelectClass(classItem)} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">
+                Select
+              </button>
+            ) : (
+              <button className="bg-gray-500 cursor-not-allowed text-white px-4 py-2 rounded-lg" disabled>
+                {isAdmin || isInstructor ? "Not Available" : "Login to Select"}
+              </button>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
